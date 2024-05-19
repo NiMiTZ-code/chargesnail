5d4f5a62cef8aa440f758d49ad72dcbff21ad396
Erdevis <bartek.natkaniec@vp.pl>
2024-05-19 23:38:41 +0200

    css fixing

--- electric_chargers/src/components/home.jsx
+++ electric_chargers/src/components/home.jsx
@@ -7,23 +7,21 @@ import { logo, map, fb, lin, yt, ins } from '../images';
 function Home() {
   return (
     <>
-      <div className="menu"> 
-        <Link to="/" id='logo-image'><img src={logo} alt="logo" id='logo'/></Link>
-        <div className="menu_bar">
+      <div className="menu">
+        <img src={logo} alt="logo" id='logo'/>
+        
           <ul id='menu-items'>
-            <li id='login'><Link to="/login">Log in</Link></li>
+            <li><Link to="/login">Log in</Link></li>
             <li><Link to="/register">Sign up</Link></li>
           </ul>
-        </div>
+        
       </div>
       <main>
         <section>
           <h1>About us</h1>
-          <p>Join us on the journey to a greener tomorrow with E-Parker – where every charge propels us towards a cleaner, more sustainable future.<br /></p>
-          <p>At E-Parker, we’re driving the future of sustainable transportation by offering state-of-the-art electric vehicle (EV) charging solutions. Our mission is to empower EV owners with convenient, 
-            reliable, and accessible charging infrastructure, making electric mobility the norm, not the exception.<br /></p>
-          <p>Founded on the principles of innovation and environmental stewardship, E-Parker provides a seamless charging experience through a network of high-performance chargers. We’re committed to accelerating
-            the transition to clean energy vehicles by expanding our footprint of fast-charging stations across urban and rural landscapes.</p>
+          <p>Join us on the journey to a greener tomorrow with E-Parker – where every charge propels us towards a cleaner, more sustainable future.</p>
+          <p>At E-Parker, we’re driving the future of sustainable transportation by offering state-of-the-art electric vehicle (EV) charging solutions. Our mission is to empower EV owners with convenient, reliable, and accessible charging infrastructure, making electric mobility the norm, not the exception.</p>
+          <p>Founded on the principles of innovation and environmental stewardship, E-Parker provides a seamless charging experience through a network of high-performance chargers. We’re committed to accelerating the transition to clean energy vehicles by expanding our footprint of fast-charging stations across urban and rural landscapes.</p>
         </section>
         <div className="mapa">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d81630.22253703541!2d18.880846297265617!3d50.2556217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716ce4ab183674f%3A0xf5b0bf7dd50cb457!2sWydzia%C5%82%20In%C5%BCynierii%20Materia%C5%82owej%20Politechniki%20%C5%9Al%C4%85skiej!5e0!3m2!1spl!2spl!4v1716035646128!5m2!1spl!2spl" width="600" height="450" loading="lazy" allowFullScreen={true} referrerPolicy="no-referrer-when-downgrade"></iframe>
@@ -39,7 +37,6 @@ function Home() {
             <li><img src={ins} alt="instagram" /></li>
           </ul>
         </div>
-        <div></div>
       </footer>
     </>
   );
