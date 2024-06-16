7d47f38a86ec5ea62eea189c1539a3c899dfbef2
Dawid Niemiec <dn305391@student.polsl.pl>
2024-06-16 15:33:32 +0200

    Added isOwner of reservation check to middleware

--- Backend/middleware/authMiddleware.ts
+++ Backend/middleware/authMiddleware.ts
@@ -1,4 +1,7 @@
 import jwt from 'jsonwebtoken';
+import db from '../config/db.config.js';
+import { eq } from 'drizzle-orm';
+import { reservations } from '../models/reservation.js';
 const isLoggedUser = async (req, res, next) => {
     try {
         if (!req.headers.authorization) {
@@ -32,4 +35,19 @@ const isAdmin = async (req, res, next) => {
     }
 }
 
-export { isLoggedUser, isAdmin };
+const isOwner = async (req, res, next) => {
+    try{
+        const user_uuid = req.user.id;
+        const reservation_id = req.body.id;
+        const ownerId = await db.select({user_uuid: reservations.user_id}).from(reservations).where(eq(reservations.id, reservation_id));
+        if(ownerId !== user_uuid){
+            return res.status(401).json({error: 'Unauthorized. User does not own this reservation'});
+        }
+        return next();
+    }
+    catch(error){
+        res.status(401).json({error});
+    }
+}
+
+export { isLoggedUser, isAdmin, isOwner };
